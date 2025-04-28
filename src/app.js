13abdb6c2f0cecba95d9cd47df6100c4f5c1cc91
TheCharismaticEnigma <sde.avishkar@gmail.com>
2025-04-28 19:56:16 +0530

    Add static & cookieParser middlewares

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import { PAYLOAD_LIMITS } from "./constants";
 
+import cookieParser from "cookie-parser";
 export const expressApp = express();
 
 const { DEFAULT: DEFAULT_PAYLOAD_LIMIT } = PAYLOAD_LIMITS;
@@ -26,5 +27,8 @@ expressApp.use(
     limit: DEFAULT_PAYLOAD_LIMIT,
   })
 );
+
+expressApp.use(cookieParser());
+expressApp.use(express.static());
 
 export default expressApp;
