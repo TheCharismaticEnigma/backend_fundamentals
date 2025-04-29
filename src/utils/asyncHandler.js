4e932f67ab37e47e947defb269750a1998122e8d
TheCharismaticEnigma <sde.avishkar@gmail.com>
2025-04-29 20:00:27 +0530

    Create a generic asyncHandler wrapper function

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+export const asyncHandler =
+  (requestHandler) => async (request, response, next) => {
+    try {
+      await requestHandler(request, response, next);
+    } catch (error) {
+      response.status(error?.code || 500).json({
+        success: false,
+        message: error?.message ?? "Something went wrong",
+      });
+    }
+  };
+
+export default asyncHandler;
